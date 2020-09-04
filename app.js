39084119e82959a13f6f5e480660b44ae223c1ba
Kaustav Shouvik <kaustav.shouvik00@gmail.com>
2020-09-04 15:43:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('HELLO FROM THE OTHER SIDE!!!')
+})
+
+app.listen(5000, () => {
+    console.log(`Server started at port 5000`)
+})
